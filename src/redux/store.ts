d80eea174e06c1812daee8fac80fb83e64f88e80
ayan-akd <ayankumar.akd@gmail.com>
2025-01-09 21:39:35 +0600

    started the module

---
+++ src/redux/store.ts
@@ -0,0 +1,8 @@
+//configure redux store
+import { configureStore } from '@reduxjs/toolkit';
+export const store = configureStore({
+  reducer: {},
+});
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
